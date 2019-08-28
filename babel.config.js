b0a297f9b5af85168b964052030eada6e9fb1689
buffge <1515888956@qq.com>
2019-08-28 16:22:48 +0800

    rn升级到0.60.5

--- babel.config.js
+++ babel.config.js
@@ -1,25 +1,3 @@
 module.exports = {
-  presets: ["module:metro-react-native-babel-preset"],
-  plugins: [
-    [
-      "module-resolver",
-      {
-        //这个配置无效
-        root: ["./src/"],
-        alias: {
-          "@": "./src",
-          "@routes": "./src/routes",
-          "@config": "./src/config",
-          "@utils": "./src/utils",
-          "@pages": "./src/pages",
-          "@components": "./src/components",
-          "@styles": "./src/assets/styles",
-          "@img": "./src/assets/images",
-          "@api": "./src/services",
-          "@redux": "./src/redux",
-        },
-      },
-    ],
-    ["@babel/plugin-proposal-decorators", { legacy: true }],
-  ],
-}
+  presets: ['module:metro-react-native-babel-preset'],
+};
